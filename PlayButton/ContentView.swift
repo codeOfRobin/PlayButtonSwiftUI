c31076d22bf807ec184639955e4a0fe12c058bfb
Robin Malhotra <me@rmalhotra.com>
2020-06-24 23:26:33 +0100

    Initial Commit

---
+++ PlayButton/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PlayButton
+//
+//  Created by Robin Malhotra on 24/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayButton/PlayButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayButtonApp.swift
+//  PlayButton
+//
+//  Created by Robin Malhotra on 24/06/20.
+//
+
+import SwiftUI
+
+@main
+struct PlayButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
